027fb38c00220c96dcf332c488054f02f9cb3e23
Evelyn Almeida <evelynalmeida2004@gmail.com>
2023-11-20 16:47:24 -0400

    Correcao do caminho p/ acesso ao arq. json arduino

---
+++ javaProject/javaCrud/src/main/java/com/javaCrud/ImageIconCustom.java
@@ -0,0 +1,10 @@
+package com.javaCrud;
+
+import javax.swing.ImageIcon;
+import java.awt.Image;
+
+public class ImageIconCustom extends ImageIcon {
+    public ImageIconCustom(Image image) {
+        super(image);
+    }
+}
